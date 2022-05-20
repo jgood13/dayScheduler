61146b0baec1845b4458940b198680e925a60b02
jgood13 <joseph.good8@gmail.com>
2022-05-20 15:29:12 -0600

    added some comments, changed some js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-// $(document).ready(function(){
-
 var currentDay = moment().format('MMM Do, YYYY, hh:mm')
-$("#currentDay").text(currentDay)
-var saveBtn = $(".saveBtn")
-var description = $('.description')
+var postDate = document.querySelector('#currentDay')
 
+function changeDate(){
+    return postDate.innerHTML = currentDay
+}
+changeDate()
+// I learned if you have all the buttons called the same ID and use that for the Var it will only call on the first button
+var saveBtn = $(".saveBtn")
+
+
+// changes the color of the textarea when the clock goes past the corresponding ID
+// Going by 24 hour clock eg. 5pm = 17
 function blockColor(){
     var hour = moment().hour();
 
     $('.description').each(function() {
-        var curHour = parseInt($(this).attr('id'))
 
+        var curHour = $(this).attr('id')
         if (curHour > hour ){
-            $('.description').addClass('future')
-            
-        } else if (curHour === hour ){
-            $('.description').addClass('present')
+            $(this).addClass('future')
+        } else if (curHour == hour ){
+            $(this).addClass('present')
         } else {
-            $('.description').addClass('past')
+            $(this).addClass('past')
         }
         
     })
@@ -26,35 +31,37 @@ blockColor()
 
 function renderSaved(){
 
-    description.each(function() {
+    $('.description').each(function() {   //calling on each textarea to find a key that matches it's ID and then displays the value
     
     for (let i=0;i<localStorage.length;i++) {
 
         let keyValue = localStorage.key(i);
         let contentValue = localStorage.getItem(keyValue)
-        let hourBox = $(this).parent().attr("id");
+        let hourBox = $(this).attr("id");
 
         if (keyValue ===  hourBox){
             $(this).val(contentValue)
         }
-    }})
+    }}
+    )
 
 }
 renderSaved();
 
 function saveFunction(){
-
-    saveBtn.each(function() {
-    
-    let hourBox = $(this).parent().attr("id");
+    let hourBox = $(this).siblings(".description").attr("id"); 
     let content = $(this).siblings(".description").val();
-    console.log(hourBox);
-    console.log(content);
     
     localStorage.setItem(hourBox, content)
-})}
+}
 
-saveBtn.on("click", saveFunction)
+
+saveBtn.on("click", saveFunction) //I kept getting undefined when I added () to savefunction
+
+
+// I was trying to use saveBtn with them all the same ID which didn't work so I
+// then tried all of them different ID which is when I found out my problem.
+
 // $('#saveBtn2').on("click", saveFunction)
 // $('#saveBtn3').on("click", saveFunction)
 // $('#saveBtn4').on("click", saveFunction)
